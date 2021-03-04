c1c919ae782eeeb216b74dd083ba008ff2b99582
baiyuying <592689605@qq.com>
2021-03-04 17:42:51 +0800

    First commit, learning NSString and methods of using GitHub with Xcode.

---
+++ helloworld/NSString.h
@@ -0,0 +1,16 @@
+//
+//  NSString.m
+//  helloworld
+//
+//  Created by 白瑜颖 on 2021/3/4.
+//
+//NSString的用法简单体验
+
+#import <Foundation/Foundation.h>
+
+void printNSString()
+{
+    NSString *myString = @"Hellow world!";
+    NSLog(myString);
+    return;
+}
--- helloworld/main.m
+++ helloworld/main.m
@@ -6,11 +6,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "NSString.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        printNSString();
     }
     return 0;
 }
+
