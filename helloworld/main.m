4778a0923d8201f21e79d4834c182d73b84ceaf7
baiyuying <592689605@qq.com>
2021-03-04 15:32:41 +0800

    Initial Commit

---
+++ helloworld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  helloworld
+//
+//  Created by 白瑜颖 on 2021/3/4.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
